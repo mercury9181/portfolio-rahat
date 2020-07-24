a5ac65a9bd240027861acbc03299b2e44f575441
mercury9181 <mercury9181@gmail.com>
2020-07-24 23:56:31 +0600

    added some index style

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -3,11 +3,17 @@ import Header from '../shared/Header';
 
 const BaseLayout = (props) => {
 
+  const {className , children} = props;
+
   return (
-    <React.Fragment>
+    <div className="layout-container" >
       <Header />
-      {props.children}
-    </React.Fragment>
+      <main className={`cover ${className}`}>
+        <div className="wrapper">
+          {children}
+        </div>
+      </main>
+    </div>
   )
 }
 
--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,55 +1,114 @@
-import React from 'react';
-import Link from 'next/link';
+// import React from 'react';
+// import Link from 'next/link';
+//
+//
+//
+// class Header extends React.Component {
+//
+//
+//   render() {
+//
+//     return (
+//       <React.Fragment>
+//
+//         <Link href="/">
+//           <a style={{'fontSize': '20px'}}> Home </a>
+//         </Link>
+//
+//         <Link href="/about">
+//           <a> About </a>
+//         </Link>
+//
+//         <Link href="/portfolios">
+//           <a> Portfolio </a>
+//         </Link>
+//
+//         <Link href="/blogs">
+//           <a> Blog </a>
+//         </Link>
+//
+//         <Link href="/cv">
+//           <a> CV </a>
+//         </Link>
+//         <style jsx>
+//           {
+//           `
+//           a {
+//             font-size: 20px;
+//           };
+//           .customClass {
+//             color: red;
+//           }
+//           `
+//           }
+//         </style>
+//       </React.Fragment>
+//     )
+//   }
+// }
+//
+// export default Header;
 
-import '../../styles/main.scss';
-
-class Header extends React.Component {
 
 
-  render() {
-    const title = this.props.title;
 
-    return (
-      <React.Fragment>
-        <p> { title } </p>
-        {this.props.children}
-        <p className='customClass'> I am styled P element </p>
-        <p className='customClassFromFile'> I am styled P element </p>
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
-        <Link href="/">
-          <a style={{'fontSize': '20px'}}> Home </a>
-        </Link>
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-        <Link href="/about">
-          <a> About </a>
-        </Link>
+  const toggle = () => setIsOpen(!isOpen);
 
-        <Link href="/portfolios">
-          <a> Portfolio </a>
-        </Link>
-
-        <Link href="/blogs">
-          <a> Blog </a>
-        </Link>
-
-        <Link href="/cv">
-          <a> CV </a>
-        </Link>
-        <style jsx>
-         {
-          `
-          a {
-            font-size: 20px;
-          };
-          .customClass {
-            color: red;
-          }
-          `
-         }
-        </style>
-      </React.Fragment>
-    )
-  }
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Header;
+export default Example;
